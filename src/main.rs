31a27e6a9e16a78950cfeed288df1be819719898
Harry George <harry.m.george@gmail.com>
2026-07-19 20:51:17 +0100

    re-enable base build needed for the leptos stuff

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+//! This is the wrapper for trunk building a Leptos application.
+
+use ui::App;
+use tracing_subscriber::{filter::Targets, prelude::*};
+use tracing_web::MakeWebConsoleWriter;
+
+fn main() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(true)
+        .without_time()
+        .with_writer(MakeWebConsoleWriter::new())
+        .with_filter(
+            Targets::new()
+                .with_target("leptos", tracing::Level::DEBUG)
+                .with_default(tracing::Level::TRACE),
+        );
+
+    tracing_subscriber::registry().with(fmt_layer).init();
+    console_error_panic_hook::set_once();
+
+    tracing::info!("Starting Leptos application");
+    leptos::mount::mount_to_body(App);
+}
